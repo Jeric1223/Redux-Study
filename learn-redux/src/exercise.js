53bcbbe35639a1604e9962a18668486bf1f5659c
JaeHyun <soehd0889@naver.com>
2021-04-08 09:12:29 +0900

    [2021-04-08] Redux를 사용해서 Counter 만들어보기

--- learn-redux/src/exercise.js
+++ learn-redux/src/exercise.js
@@ -1,38 +1,39 @@
 import { createStore } from "redux";
 
-//createStore는 스토어를 만들어주는 함수입니다.
-//리액트 프로젝트에서는 단 하나의 스토어를 만들어야 합니다
+// createStore는 스토어를 만들어주는 함수입니다.
+// 리액트 프로젝트에서는 단 하나의 스토어를 만듭니다.
 
-/*리덕스에서 관리 할 정의*/
+/* 리덕스에서 관리 할 상태 정의 */
 const initialState = {
   counter: 0,
   text: "",
   list: [],
 };
 
-/*액션 타입 정의 */
-//액션 타입은 주로 대문자로 작성합니다.
+/* 액션 타입 정의 */
+// 액션 타입은 주로 대문자로 작성합니다.
 const INCREASE = "INCREASE";
 const DECREASE = "DECREASE";
 const CHANGE_TEXT = "CHANGE_TEXT";
 const ADD_TO_LIST = "ADD_TO_LIST";
 
 /* 액션 생성함수 정의 */
-//액션 생성함수는 주로 camelCase로 작성합니다.
+// 액션 생성함수는 주로 camelCase 로 작성합니다.
 function increase() {
   return {
-    type: INCREASE,
+    type: INCREASE, // 액션 객체에는 type 값이 필수입니다.
   };
 }
 
-//화살표 함수로 작성하는 것이 더욱 코드가 간단하기에,
+// 화살표 함수로 작성하는 것이 더욱 코드가 간단하기에,
+// 이렇게 쓰는 것을 추천합니다.
 const decrease = () => ({
   type: DECREASE,
 });
 
 const changeText = (text) => ({
   type: CHANGE_TEXT,
-  text,
+  text, // 액션안에는 type 외에 추가적인 필드를 마음대로 넣을 수 있습니다.
 });
 
 const addToList = (item) => ({
@@ -40,13 +41,13 @@ const addToList = (item) => ({
   item,
 });
 
-/*리듀서 만들기*/
+/* 리듀서 만들기 */
 // 위 액션 생성함수들을 통해 만들어진 객체들을 참조하여
-//새로운 상태를 만드는 함수를 만들어 봅시다.
-//주의: 리듀서에서는 불변성을 꼭 지켜줘야 합니다!
+// 새로운 상태를 만드는 함수를 만들어봅시다.
+// 주의: 리듀서에서는 불변성을 꼭 지켜줘야 합니다!
 
 function reducer(state = initialState, action) {
-  //state의 초깃값을 initialState로 지정했습니다.
+  // state 의 초깃값을 initialState 로 지정했습니다.
   switch (action.type) {
     case INCREASE:
       return {
@@ -73,19 +74,22 @@ function reducer(state = initialState, action) {
   }
 }
 
+/* 스토어 만들기 */
 const store = createStore(reducer);
 
-console.log(store.getState()); //현재 store 안에 들어있는 상태를 조회하는 합니다.
+console.log(store.getState()); // 현재 store 안에 들어있는 상태를 조회합니다.
 
-//스토어안에 들어있는 상태가 바뀔 때 마다 호출되는 listener 함수
+// 스토어안에 들어있는 상태가 바뀔 때 마다 호출되는 listener 함수
 const listener = () => {
   const state = store.getState();
   console.log(state);
 };
 
 const unsubscribe = store.subscribe(listener);
+// 구독을 해제하고 싶을 때는 unsubscribe() 를 호출하면 됩니다.
 
+// 액션들을 디스패치 해봅시다.
 store.dispatch(increase());
 store.dispatch(decrease());
-store.dispatch(changeText('안녕하세요'));
-store.dispatch(addToList({ id: 1, text: '와우' }));
+store.dispatch(changeText("안녕하세요"));
+store.dispatch(addToList({ id: 1, text: "와우" }));
--- learn-redux/src/index.js
+++ learn-redux/src/index.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import rootReducer from "./modules";
+import { composeWithDevTools } from "redux-devtools-extension"; // 리덕스 개발자 도구
+
+const store = createStore(rootReducer, composeWithDevTools()); // 스토어를 만듭니다.
+// composeWithDevTools 를 사용하여 리덕스 개발자 도구 활성화
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </Provider>,
+  document.getElementById("root")
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+serviceWorker.unregister();
