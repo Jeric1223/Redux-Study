1844d2cb17c1f519e6d339d3101e3384ac150830
JaeHyun <soehd0889@naver.com>
2021-04-10 10:42:33 +0900

    [2021-04-10] 프리젠테이셔널 컴포넌트 구현하기

--- learn-redux/src/App.js
+++ learn-redux/src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 import CounterContainer from "./container/CounterContainer";
+import TodosContainer from "./container/TodosCoutainer";
 
 function App() {
   return (
     <div>
       <CounterContainer />
+      <hr />
+      <TodosContainer />
     </div>
   );
 }
