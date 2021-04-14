9959f0c2ace792535f8ec67538c0720fa6ed5522
JaeHyun <soehd0889@naver.com>
2021-04-14 19:35:39 +0900

    [2021-04-14] 간단하게 미들웨어 만들기

--- Redux-Middleware/learn-redux-middleware/src/index.js
+++ Redux-Middleware/learn-redux-middleware/src/index.js
@@ -2,11 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 import rootReducer from "./modules";
+import myLogger from "./middlewares/MyLogger";
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(myLogger));
 
 ReactDOM.render(
   <Provider store={store}>
