62c280ede6f86878742568e4b092be6a7f6a51f8
JaeHyun <soehd0889@naver.com>
2021-04-14 19:09:19 +0900

    [2021-04-14] 리덕스 미들웨어 기본세팅

---
+++ Redux-Middleware/learn-redux-middleware/src/containers/CounterContainer.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Counter from "../components/Counter";
+import { useSelector, useDispatch } from "react-redux";
+import { increase, decrease } from "../modules/counter";
+
+function CounterContainer() {
+  const number = useSelector((state) => state.Counter);
+  const dispatch = useDispatch();
+
+  const onIncrease = () => {
+    dispatch(increase());
+  };
+  const onDecrease = () => {
+    dispatch(decrease());
+  };
+
+  return (
+    <Counter number={number} onIncrease={onIncrease} onDecrease={onDecrease} />
+  );
+}
+
+export default CounterContainer;
---
+++ Redux-Middleware/learn-redux-middleware/src/modules/counter.js
@@ -0,0 +1,21 @@
+//액션 타입
+const INCREASE = "INCREASE";
+const DECREASE = "DECREASE";
+
+//액션 생성 함수
+export const increase = () => ({ type: INCREASE });
+export const decrease = () => ({ type: DECREASE });
+
+//초기값 (상태가 객체가 아니라 그냥 숫자여도 상관 없습니다.)
+const initialState = 0;
+
+export default function counter(state = initialState, action) {
+  switch (action.type) {
+    case INCREASE:
+      return state + 1;
+    case DECREASE:
+      return state - 1;
+    default:
+      return state;
+  }
+}
